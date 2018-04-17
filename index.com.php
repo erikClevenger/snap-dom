ab2aba5d0c2d69aa92fc7158970c6aa263583d4c
Erik Clevenger <eclevenger@cnm.edu>
2018-04-17 08:47:10 -0600

    init commit

---
+++ index.com.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>SNAP!</title>
+	</head>
+	<body>
+		<header>
+			<span>kitty shame</span>
+			<nav>
+				<ul>
+					<li>here</li>
+					<li>there</li>
+					<li>thurr</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<center><p>lul@ deprecated centering</p></center>
+			<div class="whoa">
+				<span><h2>WHOA!</h2></span>
+				<iframe src="https://i.imgur.com/p3LXvJe.png" frameborder="0"></iframe>
+			</div>
+
+		</section>
+		<section>
+			<button>
+				<h4>DO NOT PRESS</h4>
+			</button>
+		</section>
+		<section>
+			<p>srsly. Don't press that.</p>
+		</section>
+		<footer>
+			<nav>
+				<ul>
+					<li>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+
+	</body>
+</html>
